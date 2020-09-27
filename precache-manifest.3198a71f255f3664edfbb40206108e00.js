bd61183c2c064ebbda00312f1039eb53e24f3675
Artyom <Psixart1@gmail.com>
2020-09-27 16:46:02 +0300

    Updates

---
+++ precache-manifest.3198a71f255f3664edfbb40206108e00.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a24c49857750a46423ee48901b5e27",
+    "url": "/CorexTZ/index.html"
+  },
+  {
+    "revision": "4d0b79500a4cb2122398",
+    "url": "/CorexTZ/static/css/main.fb9d9f5e.chunk.css"
+  },
+  {
+    "revision": "4e97e077326575b7e888",
+    "url": "/CorexTZ/static/js/2.551008cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CorexTZ/static/js/2.551008cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0b79500a4cb2122398",
+    "url": "/CorexTZ/static/js/main.736a6c0a.chunk.js"
+  },
+  {
+    "revision": "04806a80dbd9d90bec56",
+    "url": "/CorexTZ/static/js/runtime-main.0375c09e.js"
+  },
+  {
+    "revision": "f85bf7d18134e84ed1ad74ba1ae8e9eb",
+    "url": "/CorexTZ/static/media/14.f85bf7d1.png"
+  },
+  {
+    "revision": "ac758c81562127f2b1fd9ec880dedebe",
+    "url": "/CorexTZ/static/media/arrow.ac758c81.svg"
+  },
+  {
+    "revision": "ccfec9621b8cfdad80971c9b0d413020",
+    "url": "/CorexTZ/static/media/burger.ccfec962.svg"
+  },
+  {
+    "revision": "d47dc684a3fd217b38be2e8b53991326",
+    "url": "/CorexTZ/static/media/cart.d47dc684.svg"
+  },
+  {
+    "revision": "1f3c989def9dad9a4b5e08edcefc912f",
+    "url": "/CorexTZ/static/media/copml-dot.1f3c989d.svg"
+  },
+  {
+    "revision": "82eeb76e9dd6ffdd7c9dd060084d49a4",
+    "url": "/CorexTZ/static/media/fb.82eeb76e.svg"
+  },
+  {
+    "revision": "84589b3e85a4760f2a1d769e64ef5584",
+    "url": "/CorexTZ/static/media/img-in-new.84589b3e.svg"
+  },
+  {
+    "revision": "659bb13340018f413bc24304b991bca2",
+    "url": "/CorexTZ/static/media/logo.659bb133.svg"
+  },
+  {
+    "revision": "fddaa9e213c0e587d2e16a983a2c4cb4",
+    "url": "/CorexTZ/static/media/news-logo.fddaa9e2.svg"
+  },
+  {
+    "revision": "1caf95cec76814c4e329fab1287ed3f2",
+    "url": "/CorexTZ/static/media/null-dot.1caf95ce.svg"
+  },
+  {
+    "revision": "51eb20d264d1645179e6ba8899640258",
+    "url": "/CorexTZ/static/media/planet.51eb20d2.svg"
+  },
+  {
+    "revision": "918b942ee143932880481ade957b281c",
+    "url": "/CorexTZ/static/media/rd.918b942e.svg"
+  },
+  {
+    "revision": "8cb193d32a8a63f27e3f620e86bca450",
+    "url": "/CorexTZ/static/media/smallLogo.8cb193d3.svg"
+  },
+  {
+    "revision": "dd08b639c36405642236c58e8aa5ec6f",
+    "url": "/CorexTZ/static/media/star.dd08b639.svg"
+  },
+  {
+    "revision": "55307c3a4397db3e816d262f770e359d",
+    "url": "/CorexTZ/static/media/tw.55307c3a.svg"
+  },
+  {
+    "revision": "55307c3a4397db3e816d262f770e359d",
+    "url": "/CorexTZ/static/media/you.55307c3a.svg"
+  }
+]);
